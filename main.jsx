a4693456b4888751f7d840c4e6beb9e44adddbb7
povk35808 <povk35808@gmail.com>
2025-11-06 17:50:30 +0700

    Create main.jsx

---
+++ main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// ហៅ File ទី 3 របស់អ្នក (App.jsx)
+import App from './App.jsx'
+// ហៅ File ទី 4 របស់អ្នក (index.css)
+import './index.css'
+// ហៅ File ទី 5 របស់អ្នក (style.css)
+import './style.css' 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
